9f1e593009518e16a566de44584c3ceb73e6facc
Tafarel Gasparin <tafareltec@gmail.com>
2026-02-06 14:05:17 -0300

    shadcn

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
+        <section className="container mx-auto px-4 py-20 text-center">
+          <h2 className="text-5xl font-bold mb-6 bg-gradient-to-r from-gray-900 to-gray-600 text-transparent bg-clip-text">O Saas mais simples do mundo</h2>
+          <p className="text-xg text-gray-600 mb-8 max-w-2xl mx-auto">Crie e gerencie suas tarefas de forma fácil e rápida.</p>
+        </section>
+    </div>
+  );
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,11 @@
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <h1>Welcome to the Home Page</h1>
+        <Button variant="outline">Click Me</Button>
     </div>
   );
 }
